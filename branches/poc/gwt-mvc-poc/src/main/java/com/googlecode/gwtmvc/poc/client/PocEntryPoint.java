936aed54bc1b3b90a500358f0d672f474be93a05
wokier <wokier@535e96a7-9241-0410-80e2-f1163fee2ef0>
2008-08-06 06:49:42 +0000

    Initial import.

---
+++ branches/poc/gwt-mvc-poc/src/main/java/com/googlecode/gwtmvc/poc/client/PocEntryPoint.java
@@ -0,0 +1,12 @@
+package com.googlecode.gwtmvc.poc.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class PocEntryPoint implements EntryPoint {
+
+	public void onModuleLoad() {
+		Window.alert("Hello World");
+	}
+
+}
