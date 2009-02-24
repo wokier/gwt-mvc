c9c1564511f9ce2c074520b6cf8d7a3f9ed88476
wokier <wokier@535e96a7-9241-0410-80e2-f1163fee2ef0>
2009-02-24 12:34:34 +0000

    Lazy Panel

--- trunk/gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/View.java
+++ trunk/gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/View.java
@@ -1,6 +1,7 @@
 package com.googlecode.gwtmvc.client;
 
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.widgetideas.client.LazyPanel;
 
 /**
  * The view is the graphical part of the application get datas from model use
@@ -9,8 +10,9 @@ import com.google.gwt.user.client.ui.Composite;
  * USAGE: The view could be implemented by a component or a group of components
  * The contructor must be call by the controller of the view.
  * @param <T> type
+ * @param <W> content widget
  */
-public abstract class View<T> extends Composite implements IView<T> {
+public abstract class View<T, W extends Widget> extends LazyPanel<W> implements IView<T> {
 
 	protected String key;
 
@@ -37,24 +39,30 @@ public abstract class View<T> extends Composite implements IView<T> {
 			model.addListener(this);
 		}
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.googlecode.gwtmvc.client.IModelListener#onModelChange(com.googlecode.gwtmvc.client.ModelForView)
-	 */
-	public abstract void onModelChange(ModelForView<T> model) ;
 	
-	/**
-	 * @see com.googlecode.gwtmvc.client.IView#init()
-	 */
-	public abstract void init();
-
 	/**
 	 * @see com.googlecode.gwtmvc.client.IView#getKey()
 	 */
 	public String getKey() {
 		return key;
 	}
+	
+	/**
+	 * @see com.googlecode.gwtmvc.client.IView#init()
+	 * @deprecated replaced by lazyPanel
+	 */
+	public abstract void init();
+	
+	/**
+	 * @see com.google.gwt.widgetideas.client.LazyPanel#createWidget()
+	 */
+	@Override
+	public abstract W createWidget() ;
+	
+	/**
+	 * @see com.googlecode.gwtmvc.client.IModelListener#onModelChange(com.googlecode.gwtmvc.client.ModelForView)
+	 */
+	public abstract void onModelChange(ModelForView<T> model) ;
 	
 	@Override
 	public String toString() {
