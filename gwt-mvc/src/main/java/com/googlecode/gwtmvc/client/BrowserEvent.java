c195eebbad8dc238856ffd8437531d5674c0bed9
wokier <wokier@535e96a7-9241-0410-80e2-f1163fee2ef0>
2009-07-23 08:21:13 +0000

    url params

--- gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/BrowserEvent.java
+++ gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/BrowserEvent.java
@@ -8,7 +8,8 @@ import com.google.gwt.user.client.History;
 /**
  * Event sent by the browser to signal a new URL or historyToken.<br />
  * The GWT historyToken system allows to follow navigation history and to have
- * bookmarkable pages.<br />The historyToken is decomposed into action and params.<br />
+ * bookmarkable pages.<br />
+ * The historyToken is decomposed into action and params.<br />
  * Ex: url#action?param1=value1&param2=value2
  * 
  * @see History
@@ -17,9 +18,9 @@ public class BrowserEvent {
 
 	private String action;
 	private Map<String, String> params;
-	
+
 	private static final String URL_PARAMS_START = "?";
-	private static final char URL_PARAMS_EQUAL = '=';
+	private static final String URL_PARAMS_EQUAL = "=";
 	private static final String URL_PARAMS_SEPARATOR = "&";
 
 	/**
@@ -34,6 +35,7 @@ public class BrowserEvent {
 			params = toMap(historyToken.substring(historyToken.indexOf(URL_PARAMS_START) + 1).trim());
 		} else {
 			action = historyToken.trim();
+			params = new HashMap<String, String>();
 		}
 	}
 
@@ -41,7 +43,10 @@ public class BrowserEvent {
 		Map<String, String> paramsMap = new HashMap<String, String>();
 		String[] paramsarray = params.split(URL_PARAMS_SEPARATOR);
 		for (String param : paramsarray) {
-			paramsMap.put(param.substring(0, param.indexOf(URL_PARAMS_EQUAL)), param.substring(param.indexOf('=') + 1));
+			if (param.contains(URL_PARAMS_EQUAL)) {
+				paramsMap.put(param.substring(0, param.indexOf(URL_PARAMS_EQUAL)), param.substring(param
+						.indexOf(URL_PARAMS_EQUAL) + 1));
+			}
 		}
 		return paramsMap;
 	}
@@ -57,6 +62,7 @@ public class BrowserEvent {
 
 	/**
 	 * Give the history token params
+	 * 
 	 * @return
 	 */
 	public Map<String, String> getParams() {
