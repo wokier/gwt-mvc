2aa1eb772bd72742d6d071bd0a81e2102fdc0a68
wokier <wokier@535e96a7-9241-0410-80e2-f1163fee2ef0>
2009-07-28 14:33:42 +0000

    Simple Exception Handling

--- trunk/gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/ModelForView.java
+++ trunk/gwt-mvc/src/main/java/com/googlecode/gwtmvc/client/ModelForView.java
@@ -1,7 +1,8 @@
 package com.googlecode.gwtmvc.client;
 
 /**
- * ModelForView allows view to call getValue and only getValue on a model.
+ * ModelForView allows view to call getValue and only getValue on a model.<br />
+ * The error field is available to notify an exception to the view, but you could also handle the error in the model directly. 
  * 
  * @param <D>
  *            data type
@@ -14,5 +15,17 @@ public interface ModelForView<D> {
 	 * @return
 	 */
 	public abstract D getValue();
+	
+	/**
+	 * Give the last server call error if exists.
+	 * @return
+	 */
+	public Throwable getError();
 
+	/**
+	 * Allows to check if the last server call had thrown an error
+	 * @return
+	 */
+	public boolean hasError();
+	
 }
